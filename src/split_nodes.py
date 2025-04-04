7130512f0cb70cbdee4bb8df760c410a5b537b0a
hartske <keatoru@gmail.com>
2025-04-03 18:38:04 -0700

    wrote text_to_textnodes(). refined split_nodes_image() and split_nodes_link()

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -34,15 +34,24 @@ def split_nodes_image(old_nodes):
             new_nodes.append(node)
             continue
 
-        text_split = list(filter(None, re.split(r"\!\[|\)", node.text)))
+        matches = re.finditer(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
 
-        for i in range(len(text_split)):
-            image_chunk = text_split[i]
-            if i % 2 == 0:
-                new_nodes.append(TextNode(image_chunk, TextType.TEXT))
-            else:
-                image_split = re.split(r"\]\(", image_chunk)
-                new_nodes.append(TextNode(image_split[0], TextType.IMAGE, image_split[1]))
+        last_index = 0
+        for match in matches:
+            # Add text before the image pattern (if any).
+            if last_index < match.start():
+                new_nodes.append(TextNode(node.text[last_index:match.start()], TextType.TEXT))
+            
+            # Add the matched image node.
+            image_text = match.group(1)  # The text inside [ ]
+            image_url = match.group(2)   # The URL inside ( )
+            new_nodes.append(TextNode(image_text, TextType.IMAGE, image_url))
+            
+            last_index = match.end()
+        
+        # Add any remaining text after the last match.
+        if last_index < len(node.text):
+            new_nodes.append(TextNode(node.text[last_index:], TextType.TEXT))
                 
     return new_nodes
 
@@ -56,13 +65,32 @@ def split_nodes_link(old_nodes):
             new_nodes.append(node)
             continue
 
-        text_split = list(filter(None, re.split(r"\[|\)", node.text)))
+        matches = re.finditer(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", node.text)
 
-        for i in range(len(text_split)):
-            link_chunk = text_split[i]
-            if i % 2 == 0:
-                new_nodes.append(TextNode(link_chunk, TextType.TEXT))
-            else:
-                link_split = re.split(r"\]\(", link_chunk)
-                new_nodes.append(TextNode(link_split[0], TextType.LINK, link_split[1]))
+        last_index = 0
+        for match in matches:
+            # Add text before the link pattern (if any).
+            if last_index < match.start():
+                new_nodes.append(TextNode(node.text[last_index:match.start()], TextType.TEXT))
+            
+            # Add the matched link node.
+            link_text = match.group(1)  # The text inside [ ]
+            link_url = match.group(2)   # The URL inside ( )
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            
+            last_index = match.end()
+        
+        # Add any remaining text after the last match.
+        if last_index < len(node.text):
+            new_nodes.append(TextNode(node.text[last_index:], TextType.TEXT))
+
+    return new_nodes
+
+def text_to_textnodes(text):
+    new_nodes = [TextNode(text, TextType.TEXT)]
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
     return new_nodes
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -77,6 +77,7 @@ class TestDelimiter(unittest.TestCase):
         assert new_nodes[6].text_type == TextType.TEXT
         
     # Split Nodes Image Tests
+class TestSplitImage(unittest.TestCase):
     def test_split_images(self):
         # Test Case 1
         node = TextNode(
@@ -157,6 +158,7 @@ class TestDelimiter(unittest.TestCase):
         )
 
     # Split Nodes Link Tests
+class TestSplitLink(unittest.TestCase):
     def test_split_link(self):
         # Test Case 1
         node = TextNode(
@@ -236,6 +238,28 @@ class TestDelimiter(unittest.TestCase):
                 TextNode("link", TextType.LINK, "https://google.com"),
                 TextNode("This is text with an ![image](https://google.com)", TextType.TEXT),
                 TextNode("image", TextType.IMAGE, "https://google.com"),
+            ],
+            new_nodes
+        )
+
+    # Text to Text Node Test
+class TestTexttoTextNode(unittest.TestCase):
+    def test_text_to_TextNode(self):
+        # Test Case 1
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
             ],
             new_nodes
         )

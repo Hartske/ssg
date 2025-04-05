18b6ae822b685f9d55373df9ab6c136a5f422284
hartske <keatoru@gmail.com>
2025-04-05 06:55:25 -0700

    replaced HTMLNode with appropriate ParentNode or LeafNode

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -7,7 +7,7 @@ from markdown_to_blocks import *
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-    parent_node = HTMLNode("div", None, None, [])
+    parent_node = ParentNode("div", [])
 
     for block in blocks:
         block_type = block_to_block_type(block)
@@ -16,12 +16,12 @@ def markdown_to_html_node(markdown):
             level = heading_level(block)
             text = re.sub(r"^#{1,6}\s+", '', block)
             children = text_to_children(text)
-            heading_node = HTMLNode(f"h{level}", None, None, children)
+            heading_node = ParentNode(f"h{level}", children)
             parent_node.children.append(heading_node)
         # Paragraph
         elif block_type is BlockType.PARAGRAPH:
             children = text_to_children(block)
-            paragraph_node = HTMLNode(f"p", None, None, children)
+            paragraph_node = ParentNode(f"p", children)
             parent_node.children.append(paragraph_node)
         # Code
         elif block_type is BlockType.CODE:
@@ -30,26 +30,26 @@ def markdown_to_html_node(markdown):
                 text = text[3:-3].strip()
             text_node = TextNode(text, TextType.TEXT)
             content = text_node_to_html_node(text_node)
-            code_node = HTMLNode('code', None, None, [content])
-            pre_node = HTMLNode('pre', None, None, [code_node])
+            code_node = ParentNode('code', [content])
+            pre_node = ParentNode('pre', [code_node])
             parent_node.children.append(pre_node)
         # Quote
         elif block_type is BlockType.QUOTE:
             text = re.sub(r'^>\s{0,}', '', block, flags=re.MULTILINE)
             children = line_breaks(text)
-            quote_node = HTMLNode('blockquote', None, None, children)
+            quote_node = ParentNode('blockquote', children)
             parent_node.children.append(quote_node)
         # Unordered List
         elif block_type is BlockType.UNORDERED_LIST:
             text = re.sub(r'^-\s{0,}', '', block, flags=re.MULTILINE)
             items = text_to_list_item(text)
-            unordered_node = HTMLNode('ul', None, None, items)
+            unordered_node = ParentNode('ul', items)
             parent_node.children.append(unordered_node)
         # Ordered List
         elif block_type is BlockType.ORDERED_LIST:
             text = re.sub(r'^\d+.\s{0,}', '', block, flags=re.MULTILINE)
             items = text_to_list_item(text)
-            ordered_node = HTMLNode('ol', None, None, items)
+            ordered_node = ParentNode('ol', items)
             parent_node.children.append(ordered_node)
     return parent_node
 
@@ -73,7 +73,7 @@ def text_to_list_item(text):
         for node in item_nodes:
             list_node = text_node_to_html_node(node)
             html_nodes.append(list_node)
-        list_item = HTMLNode('li', None, None, html_nodes)
+        list_item = ParentNode('li', html_nodes)
         list_nodes.append(list_item)
     return list_nodes
 
@@ -84,6 +84,6 @@ def line_breaks(text):
         line_children = text_to_children(line)
         all_children.extend(line_children)
         if i < len(lines) - 1:
-            br_node = HTMLNode('br', None, None, [])
+            br_node = LeafNode('br', '')
             all_children.append(br_node)
     return all_children

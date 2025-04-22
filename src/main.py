beb5924c2603462e1141deabe0e12dc4b4ca858e
hartske <keatoru@gmail.com>
2025-04-21 20:57:54 -0700

    Wrote copystatic.py and a function test in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from textnode import *
 from htmlnode import *
+from copystatic import copy_files
+import os
+import shutil
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    test = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
-    print(test)
-    test2 = LeafNode("p", "Hello world!")
-    print(test2.to_html())
+    print("~ Checking for Public Dir")
+    if os.path.exists(dir_path_public):
+        print("~ Cleaning Up")
+        shutil.rmtree(dir_path_public)
+    else:
+        print("~ All Clear!")
+
+    print("~ Copying Files to Public...")
+    copy_files(dir_path_static, dir_path_public)
 
 main()
